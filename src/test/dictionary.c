199c0b94413a4c957e6353c8a2fc1dd9df4c9c8e
Mason Demerais <mason.demerais@usask.ca>
2019-02-22 14:46:06 -0600

    tests dict

---
+++ src/test/dictionary.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#include <assert.h>
+
+#include "../chelper/common.h"
+#include "../chelper/arraylist.h"
+#include "../chelper/dictionary.h"
+#include "../chelper/string.h"
+
+int main(void)
+{
+    return 0;
+}
