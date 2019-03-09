962731ed9061270b816ddb50d54a94b3e4e18b10
Mason Demerais <mason.demerais@usask.ca>
2019-03-09 14:02:26 -0600

    Added headers for queue and stack

---
+++ src/chelper/queue.h
@@ -0,0 +1,41 @@
+#ifndef __CHELPERQUEUE__
+#define __CHELPERQUEUE__
+
+#include <stdbool.h>
+#include <stdlib.h>
+
+/* This is the Queue. */
+typedef struct Queue *Queue;
+
+#include "common.h"
+#include "arraylist.h"
+#include "string.h"
+
+/*Creates a new Queue*/
+Queue QueueCreate(free_fn_t free_fn, copy_fn_t copy_fn);
+
+/* Copies the Queue. */
+Queue QueueCopy(Queue h);
+
+/*Frees the Queue*/
+void QueueFree(Queue h);
+
+/*CLears the Queue*/
+void QueueClear(Queue h);
+
+/*Gets the amount of elements in the Queue.*/
+size_t QueueLength(Queue h);
+
+/*Gets the item in the Queue*/
+void *QueueGet(Queue h);
+
+/*Removes the item from the Queue*/
+bool QueueRemove(Queue h);
+
+/*Adds the item to the Queue*/
+bool QueueAdd(Queue h, int key, void *data);
+
+/*Removes from Queue, no free*/
+bool QueueRemoveNoFree(Queue h);
+
+#endif /* __CHELPERQUEUE__ */
---
+++ src/chelper/stack.h
@@ -0,0 +1,41 @@
+#ifndef __CHELPERSTACK__
+#define __CHELPERSTACK__
+
+#include <stdbool.h>
+#include <stdlib.h>
+
+/* This is the Stack. */
+typedef struct Stack *Stack;
+
+#include "common.h"
+#include "arraylist.h"
+#include "string.h"
+
+/*Creates a new Stack*/
+Stack StackCreate(free_fn_t free_fn, copy_fn_t copy_fn);
+
+/* Copies the Stack. */
+Stack StackCopy(Stack h);
+
+/*Frees the Stack*/
+void StackFree(Stack h);
+
+/*CLears the Stack*/
+void StackClear(Stack h);
+
+/*Gets the amount of elements in the Stack.*/
+size_t StackLength(Stack h);
+
+/*Gets the item in the Stack*/
+void *StackGet(Stack h);
+
+/*Removes the item from the Stack*/
+bool StackRemove(Stack h);
+
+/*Adds the item to the Stack*/
+bool StackAdd(Stack h, int key, void *data);
+
+/*Removes from Stack, no free*/
+bool StackRemoveNoFree(Stack h);
+
+#endif /* __CHELPERSTACK__ */
